381741d428be90289ae3a48a03fb6af8d90f68e9
adam <adammarey51@gmail.com>
2023-01-16 12:36:55 -0700

    completed setting and testing schemas and graphql queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,7 +4,8 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = require('graphql');
 
 // Client Type
@@ -18,10 +19,46 @@ const ClientType = new GraphQLObjectType({
     phone: { type: GraphQLString }
   })
 });
+// Project Type
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find(client => client.id === parent.clientId);
+      }
+    }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find(project => project.id === args.id);
+      }
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      }
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
