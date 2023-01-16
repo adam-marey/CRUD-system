97ae69a38859fd9e727f059bab1d7dd53530e20f
adam <adammarey51@gmail.com>
2023-01-16 12:08:30 -0700

    created graphql-express app and added dummy data to test out schemas

---
+++ server/schema/sampleData.js
@@ -0,0 +1,76 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    status: 'In Progress'
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'To do list ',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    status: 'In Progress'
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'fitness app',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    status: 'In Progress'
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Dating app',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    status: 'In Progress'
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'blog app',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    status: 'In Progress'
+  }
+];
+
+const clients = [
+  {
+    id: '1',
+    name: 'Adam Marey',
+    email: 'adam@gmail.com',
+    phone: '123-345-6789'
+  },
+  {
+    id: '2',
+    name: 'John Williams',
+    email: 'john@gmail.com',
+    phone: '123-345-6789'
+  },
+  {
+    id: '3',
+    name: 'Jake Anderson',
+    email: 'jake@gmail.com',
+    phone: '123-345-6789'
+  },
+  {
+    id: '4',
+    name: 'Michale Adams',
+    email: 'mickale@gmail.com',
+    phone: '123-345-6789'
+  },
+  {
+    id: '5',
+    name: 'Sam Jackson',
+    email: 'sam@gmail.com',
+    phone: '123-345-6789'
+  }
+];
+module.exports = { projects, clients };
