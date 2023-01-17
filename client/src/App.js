c1f338b49013a63180c2c63ac6d55cff251b653c
adam <adammarey51@gmail.com>
2023-01-16 17:08:52 -0700

    fetch and delete client by mutation - and fixed cache issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import Header from "./components/components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
 import Clients from "./components/components/Clients";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql', 
-  cache: new InMemoryCache(),
+  cache,
 })
 function App() {
   return (
