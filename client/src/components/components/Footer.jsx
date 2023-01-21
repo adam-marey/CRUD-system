625f6d5a5694e5c56fb6406a6971345ce1dd8396
adam <adammarey51@gmail.com>
2023-01-21 11:56:32 -0700

    added footer and social icons

--- client/src/components/components/Footer.jsx
+++ client/src/components/components/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { SocialIcon } from 'react-social-icons';
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright © EZ-Manage 2023</p>
+      <div>
+        <span className="pr-10 cursor-pointer" target="_blank">
+          <SocialIcon
+            style={{ height: 30, width: 30, marginLeft: '0.5%' }}
+            url="https://www.linkedin.com/in/sulaiman-marey/"
+            network="linkedin"
+            target="_blank"
+          />
+        </span>
+        <span className="pr-2 cursor-pointer">
+          <SocialIcon
+            style={{ height: 30, width: 30, margin: '0.5%' }}
+            url="https://github.com/adam-marey"
+            network="github"
+            target="_blank"
+          />
+        </span>
+        <span className="pr-2 cursor-pointer">
+          <SocialIcon
+            style={{ height: 30, width: 30 }}
+            url={'mailto:marey119988@gmail.com'}
+            network="email"
+            target="_blank"
+          />
+        </span>
+        <p>Copyright © EZ-Manage 2023</p>
+      </div>
     </footer>
   );
 };
